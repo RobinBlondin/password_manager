fe4652cab2aa9fe51ba6d15fe7dacab08eb064d8
Robin Blondin <robin.blondin@protonmail.com>
2023-12-14 02:21:05 +0100

    Added some new icons and mockdata

--- src/Controller/ActionListener.java
+++ src/Controller/ActionListener.java
@@ -2,6 +2,7 @@ package Controller;
 
 import Model.PasswordManager;
 import View.HomePage;
+import View.StyleSettings;
 
 import javax.swing.*;
 import java.awt.event.ActionEvent;
@@ -26,19 +27,33 @@ public class ActionListener implements java.awt.event.ActionListener {
                 System.exit(0);
             }
             if (button.getText().equals("EDIT")) {
+                //setSelectedColors(button);
                 String platform = JOptionPane.showInputDialog("Platform");
                 String userName = JOptionPane.showInputDialog("User name");
                 String newPassword = JOptionPane.showInputDialog("New Model.Password(Leave blank if you want a generated password)");
+                //resetColors(button);
                 passwordManager.changePassword(platform, userName, newPassword);
                 homePage.refresh();
             }
 
             if (button.getText().equals("REMOVE")) {
+                setSelectedColors(button);
                 String platform = JOptionPane.showInputDialog("Platform");
                 String userName = JOptionPane.showInputDialog("User name");
+                resetColors(button);
                 passwordManager.removePasswordEntry(platform, userName);
                 homePage.refresh();
             }
         }
+    }
+
+    public void setSelectedColors(JButton button) {
+        button.setForeground(StyleSettings.getInstance().getTextColor_SELECTED());
+        button.setBackground(StyleSettings.getInstance().getBackgroundColor_SELECTED());
+    }
+
+    public void resetColors(JButton button) {
+        button.setForeground(StyleSettings.getInstance().getTextColor_DARK());
+        button.setBackground(StyleSettings.getInstance().getBackgroundColor_LIGHT());
     }
 }
